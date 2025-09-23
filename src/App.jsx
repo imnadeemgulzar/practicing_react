5764de6c5e3dd0ac0873d3371f48e057ff79ac48
imnadeemgulzar <nadeembht777@gmail.com>
2025-09-23 12:12:20 +0530

    added some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,163 +1,148 @@
-// import { useState } from "react";
-// import TodoInput from "./components/todoInput";
-// import TodoList from "./components/todoList";
-// import Filters from "./components/filters";
-// import SearchTodo from "./components/searchTodo";
+import { useEffect, useState } from "react";
+import TodoInput from "./components/todoInput";
+import TodoList from "./components/todoList";
+import Filters from "./components/filters";
+import SearchTodo from "./components/searchTodo";
 
-// function App() {
-//   const [todoList, setTodoList] = useState([]);
-//   const [filter, setFilter] = useState("All");
-//   const [searchTerm,setSearchTerm] = useState('');
+function App() {
+  const [todoList, setTodoList] = useState(() => {
+    const saved = localStorage.getItem('itemList');
+   if(!saved || saved === 'undefined'){
+    return []
+   }else{
+    return JSON.parse(saved);
+   }
+  });
+  const [filter, setFilter] = useState("All");
+  const [searchTerm,setSearchTerm] = useState('');
 
-//   const handleStateChange = (id, newStatus) => {
-//     setTodoList((prev) =>
-//       prev.map((item) =>
-//         item.id === id ? { ...item, itemStatus: newStatus } : item
-//       )
-//     );
-//   };
-//   const handleRemoveItem = (id) => {
-//     setTodoList((prev) => prev.filter((item) => item.id !== id));
-//   };
+useEffect(() => {
+localStorage.setItem('itemList', JSON.stringify(todoList))
+},[todoList])
 
-//   const addItem = (itemName) => {
-//     const newItem = {
-//       id: Date.now().toString(),
-//       itemName,
-//       itemStatus: "Pending",
-//     };
-//     setTodoList([...todoList, newItem]);
-//   };
+  const handleStateChange = (id, newStatus) => {
+    setTodoList((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, itemStatus: newStatus } : item
+      )
+    );
+  };
+  const handleRemoveItem = (id) => {
+    setTodoList((prev) => prev.filter((item) => item.id !== id));
+  };
 
-//   const filterItem = (val) => {
-//     setFilter(val);
-//   };
+  const addItem = (itemName) => {
+    const newItem = {
+      id: Date.now().toString(),
+      itemName,
+      itemStatus: "Pending",
+    };
+    setTodoList([...todoList, newItem]);
+  };
 
-//   const editItem = (id,newText) => {
-//     setTodoList((prev) =>
-//       prev.map((item) => (item.id === id ? { ...item, itemName:newText } : item))
-//     );
-//   };
+  const filterItem = (val) => {
+    setFilter(val);
+  };
 
-//   const getVisibleTodos = () => {
-//     const searched = searchTerm.trim().toLowerCase();
-//     let result =  
-//     filter === "All"
-//       ? todoList
-//       : todoList.filter((item) => item.itemStatus === filter);
+  const editItem = (id,newText) => {
+    setTodoList((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, itemName:newText } : item))
+    );
+  };
 
-//       if(searched) {
-//         result = result.filter((item) => 
-//         item.itemName.toLowerCase().includes(searched)
-//         );
-//       }
-// return result;
-//   }
+  const getVisibleTodos = () => {
+    const searched = searchTerm.trim().toLowerCase();
+    let result =  
+    filter === "All"
+      ? todoList
+      : todoList.filter((item) => item.itemStatus === filter);
 
-//   return (
-//     <>
-//       <div className="h-[100vh] bg-gray-600 py-20 text-center">
-//         <TodoInput addItem={addItem} />
-//         <Filters filterItem={filterItem} filter={filter} />
-//         <SearchTodo setSearchTerm={setSearchTerm} searchTerm={searchTerm}/>
-//         <TodoList
-//           todoList={getVisibleTodos()}
-//           handleStateChange={handleStateChange}
-//           handleRemoveItem={handleRemoveItem}
-//           editItem = {editItem}
-//         />
-//       </div>
-//     </>
-//   );
-// }
+      if(searched) {
+        result = result.filter((item) => 
+        item.itemName.toLowerCase().includes(searched)
+        );
+      }
+return result;
+  }
 
-// export default App;
-// const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}`);
+  return (
+    <>
+      <div className="h-[100vh] bg-gray-600 py-20 text-center">
+        <TodoInput addItem={addItem} />
+        <Filters filterItem={filterItem} filter={filter} />
+        <SearchTodo setSearchTerm={setSearchTerm} searchTerm={searchTerm}/>
+        <TodoList
+          todoList={getVisibleTodos()}
+          handleStateChange={handleStateChange}
+          handleRemoveItem={handleRemoveItem}
+          editItem = {editItem}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
+///////////////// const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}`);////////////////////
 ///////////////////////////////////////////////////
 
 
 
+// import React, { useEffect, useState } from 'react'
+// import WeatherInput from './components/weatherApp/weatherInput'
+// import Weather from './components/weatherApp/weather'
+// const App = () => {
+//   const api_key = '991f242618034bfcb9841537252009'
+// const [city,setCity] = useState('london');
+// const [weather,setWeather] = useState(null);
+// const [loading,setLoading] = useState(false);
+// const [error,setError] = useState(null);
+// const [theme,setTheme] = useState(true);
 
 
+// const fetchData = async () => {
 
+//   if(!city) return;
+//   try {
+//     setLoading(true);
+//     setError(null)
+//     const res = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city}&days=5`);
+//     const data = await res.json();
+// if(data.error){
+//   setError(data.error.message);
+//   setWeather(null)
+// }else{
+//   setWeather(data);
+//   console.log(weather)
+// }
+//   } catch (error) {
+//     setError('Failed to fetch weather data')
+//   }
+//   finally {
+//     setLoading(false)
+//   }
+// }
 
+// useEffect(() => {
+//   if(city)
+// fetchData();
+// },[city])
+// const handleMode = () => {
+//   setTheme(!theme)
+// }
+//   return (
+//     <div className={`${theme && 'bg-gray-600'} bg-gray-200 py-20 text-center`}>
+//       <button className={`px-3 py-1 cursor-pointer ${theme ? 'bg-gray-200 text-gray-800' : 'bg-gray-800 text-gray-200'} `} onClick={handleMode}>Change Theme</button>
+//      <h1 className={`text-3xl ${theme ? 'text-gray-200' : 'text-gray-800'} font-bold mt-1`}>Weather App</h1>
+//       <WeatherInput setCity={setCity}/>
+//        {loading && <div className="mx-auto mt-3 h-12 w-12 rounded-full border-4 border-t-4 border-blue-500 border-t-transparent animate-spin"></div>}
+//       {error && <p className="text-red-400">{error}</p>}
+//       {weather && <Weather weather={weather} theme={theme}/>}
+//     </div>
+//   )
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import React, { useEffect, useState } from 'react'
-import WeatherInput from './components/weatherApp/weatherInput'
-import Weather from './components/weatherApp/weather'
-const App = () => {
-  const api_key = '991f242618034bfcb9841537252009'
-const [city,setCity] = useState('london');
-const [weather,setWeather] = useState(null);
-const [loading,setLoading] = useState(false);
-const [error,setError] = useState(null);
-const [theme,setTheme] = useState(true);
-
-
-const fetchData = async () => {
-
-  if(!city) return;
-  try {
-    setLoading(true);
-    setError(null)
-    const res = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city}&days=5`);
-    const data = await res.json();
-if(data.error){
-  setError(data.error.message);
-  setWeather(null)
-}else{
-  setWeather(data);
-  console.log(weather)
-}
-  } catch (error) {
-    setError('Failed to fetch weather data')
-  }
-  finally {
-    setLoading(false)
-  }
-}
-
-useEffect(() => {
-  if(city)
-fetchData();
-},[city])
-const handleMode = () => {
-  setTheme(!theme)
-}
-  return (
-    <div className={`${theme && 'bg-gray-600'} bg-gray-200 py-20 text-center`}>
-      <button className={`px-3 py-1 cursor-pointer ${theme ? 'bg-gray-200 text-gray-800' : 'bg-gray-800 text-gray-200'} `} onClick={handleMode}>Change Theme</button>
-     <h1 className={`text-3xl ${theme ? 'text-gray-200' : 'text-gray-800'} font-bold mt-1`}>Weather App</h1>
-      <WeatherInput setCity={setCity}/>
-       {loading && <div className="mx-auto mt-3 h-12 w-12 rounded-full border-4 border-t-4 border-blue-500 border-t-transparent animate-spin"></div>}
-      {error && <p className="text-red-400">{error}</p>}
-      {weather && <Weather weather={weather} theme={theme}/>}
-    </div>
-  )
-}
-
-export default App
+// export default App
 
 
 
--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -6,7 +6,7 @@ const TodoList = ({todoList,handleStateChange,handleRemoveItem,editItem,searchTo
     <div>
       <h2 className='text-white text-xl mt-6'>Todo List</h2>
       <div className='flex flex-wrap'>
-      {todoList.length ? todoList.map((item) => {
+      {todoList?.length ? todoList.map((item) => {
         return <TodoItem key={item.id} item={item} handleStateChange={handleStateChange} handleRemoveItem={handleRemoveItem} editItem={editItem}/>
       }
     ) : <h1 className='text-white text-4xl mt-6 mx-auto'>No Tasks Available</h1> }
